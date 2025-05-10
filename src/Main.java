b4a713887bafebf001ef97188abffd33627cb193
Sujal Sahu <95114364+sujal18ss@users.noreply.github.com>
2025-05-10 22:29:33 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 import java.util.Date;
 
-public class Main {
+public class Time {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, Docker! Current date: " + currentDate);
+    }
+}
+
+// This Main Class will show Kolkata Time zone 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+
+        // Define date format
+        SimpleDateFormat sdf = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy");
+
+        // Set to IST (Indian Standard Time)
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+
+        System.out.println("Hello, Docker! Current date: " + sdf.format(currentDate));
     }
 }
